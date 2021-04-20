e50d9411a9bfc98570409dcb109537508c534eb5
Mohammad Kamrul Hasan <kamrulhasan59246@gmail.com>
2021-04-20 04:45:34 +0000

    Did

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const psi = require('psi');
 
 const options = {
   key: `${process.env.PSI_KEY}`,
+  strategy: `mobile`,
+  locale: `en_US`
 }
 
-(async () => {
+const psigen = async (urlArray) => {
   // Get the PageSpeed Insights report
-  await psi.output('https://www.cburst.ml', { key: "AIzaSyASpq2q_p--iGDNtdYa1odhrvx7TGFMIY0"})
-  const data2 = await psi('https://theverge.com', {
-    nokey: 'true',
-    strategy: 'desktop'
-  });
-  console.log('Speed score:', data2.data.lighthouseResult.categories.performance.score);
-})();
+  const report = await psi.output('https://www.cburst.ml', options);
+  return report
+  
+};
+
+psigen(5)
